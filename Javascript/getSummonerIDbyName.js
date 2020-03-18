0a23d91e917ad5b2a7c7d551743b453ce99e4fff
Gaiall <nathan.manson.72@gmail.com>
2020-03-19 00:47:56 +0100

    le summonerName

--- Javascript/getSummonerIDbyName.js
+++ Javascript/getSummonerIDbyName.js
@@ -1,8 +1,10 @@
-var summonerName = "Talrem";
-var request = new XMLHttpRequest();
-request.open("GET","https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+summonerName+"?api_key="+apiKey,false);
-request.onload=function(){
-  var data = JSON.parse(this.response);
-  document.getElementById("summonerID").innerHTML=data.id;
-};
-request.send();
+document.getElementById("boutonSearch").onclick = function(){
+    summonerName = document.getElementById("summonerNameContainer").value;
+    var request = new XMLHttpRequest();
+    request.open("GET","https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+summonerName+"?api_key="+apiKey,false);
+    request.onload=function(){
+      var data = JSON.parse(this.response);
+      summonerId = data.id;
+    };
+    request.send();
+}
