8c80f2174611efb9c71b3281df6123d51b927d56
Talrem <43709968+Talrem@users.noreply.github.com>
2020-03-18 22:40:21 +0100

    Add files via upload

--- Javascript/loadStatus.js
+++ Javascript/loadStatus.js
@@ -1,1 +1,9 @@
-
+var apiKey = "RGAPI-0952a012-c842-4fba-aa47-eafb5fa73bd0";
+var request = new XMLHttpRequest();
+request.open("GET","https://euw1.api.riotgames.com/lol/status/v3/shard-data?api_key="+apiKey,false);
+request.onload=function(){
+  var data = JSON.parse(this.response);
+  /*Permet d'afficher le status des serveurs avec la première lettre en majuscule*/
+  document.getElementById('servStatusContainer').innerHTML=data.services[0].status.charAt(0).toUpperCase() + data.services[0].status.slice(1);
+};
+request.send();
