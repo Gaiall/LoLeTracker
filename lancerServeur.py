78d8cd6719c4f11bbf9bd5a72c65606cf1c6e7c8
Gaiall <nathan.manson.72@gmail.com>
2020-03-19 00:40:58 +0100

    le python

--- lancerServeur.py
+++ lancerServeur.py
@@ -1,3 +1,7 @@
 import os
+import sys
 
-os.system('python -m SimpleHTTPServer')
+if sys.version_info[0] == 3 and (sys.platform == 'win32' or sys.platform == 'win64'):
+    os.system('python -m http.server')
+else
+    os.system('python -m SimpleHTTPServer')
