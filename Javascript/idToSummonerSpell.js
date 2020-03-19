2084a65e7a07cd366b44d4cd2608d8c9237328e7
David Merlat <david.merlat28@gmail.com>
2020-03-19 15:27:23 +0100

    Spectate

---
+++ Javascript/idToSummonerSpell.js
@@ -0,0 +1,18 @@
+function idToSummonerSpell(id){
+  switch(id){
+    case 1: return "SummonerBoost"; break;
+    case 3: return "SummonerExhaust"; break;
+    case 4: return "SummonerFlash"; break;
+    case 6: return "SummonerHaste"; break;
+    case 7: return "SummonerHeal"; break;
+    case 11: return "SummonerSmite"; break;
+    case 12: return "SummonerTeleport"; break;
+    case 13: return "SummonerMana"; break;
+    case 14: return "SummonerDot"; break;
+    case 21: return "SummonerBarrier"; break;
+    case 30: return "SummonerPoroRecall"; break;
+    case 31: return "SummonerPoroThrow"; break;
+    case 32: return "SummonerSnowball"; break;
+    case 39: return "SummonerSnowURFSnowball_Mark"; break;
+  }
+}
