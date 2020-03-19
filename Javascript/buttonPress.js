51fac082e498d279cff1ca623675da19f43750b2
Gaiall <nathan.manson.72@gmail.com>
2020-03-19 01:56:41 +0100

    l'arnaque

--- Javascript/buttonPress.js
+++ Javascript/buttonPress.js
@@ -1,3 +1,10 @@
+document.getElementById("summonerNameContainer").addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+      document.getElementById("boutonSearch").click();
+    }
+});
+
 document.getElementById("boutonSearch").onclick = function(){
     console.log("button");
     getSummonerIDbyName();
