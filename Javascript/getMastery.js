78cbdb74e0c04cdb722a1b0fd76cf3c75914eb3a
Gaiall <nathan.manson.72@gmail.com>
2020-03-21 21:12:36 +0100

    avancement sur la mastery

--- Javascript/getMastery.js
+++ Javascript/getMastery.js
@@ -1,3 +1,13 @@
 function getSummonerMastery(){
-    
+    var request = new XMLHttpRequest();
+    request.open("GET","https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/"+summonerId+"?api_key="+apiKey, false);
+    var data;
+    request.onload = function(){
+        data = JSON.parse(this.response);
+        console.log(data);
+    }
+    request.send();
+    if(request.status == 200){
+
+    }
 }
