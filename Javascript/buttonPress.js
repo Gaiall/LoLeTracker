fcce8695934178811d583c1f2a0d538d65afc5d0
David Merlat <david.merlat28@gmail.com>
2020-03-21 19:36:16 +0100

    d

--- Javascript/buttonPress.js
+++ Javascript/buttonPress.js
@@ -18,7 +18,6 @@ document.getElementById("boutonSearch").onclick = function(){
         //Les fonctions a appeler une fois qu'on a récupéré le nom et l'icone du summoner
         let gotId = getSummonerIDbyName();
         let playing = displaySpectator();
-
     } else { //Si non, on annule tout
         let lesBouttons = document.getElementsByClassName("tablinks");
         for (let i = 0; i < lesBouttons.length; i++) {
