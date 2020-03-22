f0f75c060aaf0b8da64813f3d78146c4076fa33b
Gaiall <nathan.manson.72@gmail.com>
2020-03-22 04:06:41 +0100

    Gros avancement sur les masteries

--- Javascript/getSummonerMastery.js
+++ Javascript/getSummonerMastery.js
@@ -9,6 +9,7 @@ function getSummonerMastery(){
     request.send();
     if(request.status == 200){
         var tableau = document.getElementById("allMastery");
+        tableau.innerHTML = "";
         for (var i = 0; i < data.length; i++) {
             //On récupère les 3 premiers meilleurs champions
             var championName = chIdToName(data[i].championId)
@@ -24,17 +25,35 @@ function getSummonerMastery(){
                     break;
                 default:
             }
-            console.log(chIdToName(data[i].championId));
             let newTr = document.createElement('tr'); //On crée la nouvelle ligne
             let newTd1 = document.createElement('td'); //Colonne de l'image du perso
                 newTd1.classList.add("masteryIcon");
                 let championImage = "DATA/img/champion/tiles/"+championName+"_0.jpg"
                 let baliseImage = document.createElement('img');
+                baliseImage.src = championImage;
                 newTd1.appendChild(baliseImage);
             newTr.appendChild(newTd1);
             let newTd2 = document.createElement('td');//Colonne du nom du perso
-            let newTd3 = document.createElement('td');
+                newTd2.classList.add("masteryName");
+                let leTexte = document.createElement('p');
+                let championTitle = getChampionTitle(championName)
+                console.log(championTitle);
+                leTexte.innerHTML = championName +" : "+getChampionTitle(championTitle);
+                newTd2.appendChild(leTexte);
+            newTr.appendChild(newTd2);
+            let newTd3 = document.createElement('td');//Colonne du niveau de maitrise
+                newTd3.classList.add("masteryLevel");
+                leTexte = document.createElement('p');
+                leTexte.innerHTML = data[i].championLevel;
+                newTd3.appendChild(leTexte);
+            newTr.appendChild(newTd3);
             let newTd4 = document.createElement('td');
+                newTd4.classList.add("masteryLevel");
+                leTexte = document.createElement('p');
+                leTexte.innerHTML = data[i].championPoints;
+                newTd4.appendChild(leTexte);
+            newTr.appendChild(newTd4);
+            tableau.appendChild(newTr);
         }
     }
 }
