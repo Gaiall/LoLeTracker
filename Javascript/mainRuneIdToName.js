435928bdeca8f29eec43d505ac85691e5b6b62c3
David Merlat <david.merlat28@gmail.com>
2020-03-22 03:41:36 +0100

    m

--- Javascript/mainRuneIdToName.js
+++ Javascript/mainRuneIdToName.js
@@ -1,7 +1,7 @@
-function runeIdToName(id){
+function mainRuneIdToName(id){
   switch(id){
-    case 8100 : return "7200_Domination"; break;
-    case 8000 : return "7201_Precision"; break;
+    case 8112 : return "Domination/Electrocute/Electrocute"; break;
+    case 8124 : return "Domination/Predator/Predator"; break;
     case 8200 : return "7202_Sorcery"; break;
     case 8300 : return "7203_Whimsy"; break;
     case 8400 : return "7204_Resolve"; break;
