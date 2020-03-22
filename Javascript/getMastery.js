a75245cd0c7f2e5ce6b6e3dc5e2a96f5da1a2f12
Gaiall <nathan.manson.72@gmail.com>
2020-03-22 03:20:21 +0100

    La couleur et les mastery

--- Javascript/getMastery.js
+++ Javascript/getMastery.js
@@ -8,6 +8,33 @@ function getSummonerMastery(){
     }
     request.send();
     if(request.status == 200){
-
+        var tableau = document.getElementById("allMastery");
+        for (var i = 0; i < data.length; i++) {
+            //On récupère les 3 premiers meilleurs champions
+            var championName = chIdToName(data[i].championId)
+            switch(i){
+                case 0:
+                    document.getElementById("firstBest").src = "DATA/img/champion/tiles/"+championName+"_0.jpg"
+                    break;
+                case 1:
+                    document.getElementById("secondBest").src = "DATA/img/champion/tiles/"+championName+"_0.jpg"
+                    break;
+                case 2:
+                    document.getElementById("thirdBest").src = "DATA/img/champion/tiles/"+championName+"_0.jpg"
+                    break;
+                default:
+            }
+            console.log(chIdToName(data[i].championId));
+            let newTr = document.createElement('tr'); //On crée la nouvelle ligne
+            let newTd1 = document.createElement('td'); //Colonne de l'image du perso
+                newTd1.classList.add("masteryIcon");
+                let championImage = "DATA/img/champion/tiles/"+championName+"_0.jpg"
+                let baliseImage = document.createElement('img');
+                newTd1.appendChild(baliseImage);
+            newTr.appendChild(newTd1);
+            let newTd2 = document.createElement('td');//Colonne du nom du perso
+            let newTd3 = document.createElement('td');
+            let newTd4 = document.createElement('td');
+        }
     }
 }
