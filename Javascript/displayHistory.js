8f05e8c3530f4141d019da0ea4ce7bea154ac80a
David Merlat <david.merlat28@gmail.com>
2020-03-25 00:16:13 +0100

    d

--- Javascript/displayHistory.js
+++ Javascript/displayHistory.js
@@ -182,7 +182,7 @@ function displayHistory(){
           /*KDA*/
           res += '<td>'+data.participants[i].stats.kills+"/"+data.participants[i].stats.deaths+"/"+data.participants[i].stats.assists+'</td>';
           /*Runes TODO*/
-          res += '<td><img src="DATA/img/perk-images/Styles/'+mainRuneIdToName(data.participants[i].stats.perk1)+'.png" class="runes" alt="'+mainRuneIdToName(data.participants[i].stats.perk1)+'"><img src="DATA/img/perk-images/Styles/'+runeIdToName(data.participants[i].stats.perkSubStyle)+'.png" class="runes" alt="'+runeIdToName(data.participants[i].stats.perkSubStyle)+'"></td>';
+          res += '<td><img src="DATA/img/perk-images/Styles/'+mainRuneIdToName(data.participants[i].stats.perk0)+'.png" class="runes" alt="'+mainRuneIdToName(data.participants[i].stats.perk0)+'"><img src="DATA/img/perk-images/Styles/'+runeIdToName(data.participants[i].stats.perkSubStyle)+'.png" class="runes" alt="'+runeIdToName(data.participants[i].stats.perkSubStyle)+'"></td>';
           /*Summoner Spells*/
           if(data.participants[i].spell1Id == 0){
               srcSpell1 = "img/SpellPlaceholder";
