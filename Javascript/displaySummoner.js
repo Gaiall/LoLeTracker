6c73d7704f9780d27d45ed077d98707b1da61687
David Merlat <david.merlat28@gmail.com>
2020-03-30 15:20:23 +0200

    Final Commit I guess

--- Javascript/displaySummoner.js
+++ Javascript/displaySummoner.js
@@ -7,6 +7,14 @@ function capitalize(s) {
     return s
 }
 
+function isIn(tab, argument) {
+  let i;
+  for(i = 0 ; i < tab.length;i++){
+    if(argument == tab[i]) return true;
+  }
+  return false;
+}
+
 function displaySummoner(){
     var request = new XMLHttpRequest();
     request.open("GET", league_v4+summonerId+"?api_key="+apiKey, false);
@@ -60,6 +68,10 @@ function displaySummoner(){
         }
         request.send();
     }
+
+    let playerID = 0;
+
+    let winL10 = 0;
     let kdaL10 = "";
     let kL10 = 0;
     let dL10 = 0;
@@ -67,7 +79,13 @@ function displaySummoner(){
     let minionsL10 = 0;
     let champLvL10 = 0;
     let ccL10 = 0;
-    let playerID = 0;
+    let dmgL10 = 0;
+    let dmgL10R = 0;
+    let goldL10 = 0;
+    let visionL10 = 0;
+    let champsUsed = new Array();
+    let champsTreated = new Array();
+    let champsTreatedRate = new Array();
     for(let k = 0; k < last10length; k++){
         for(let j = 0; j < 10; j++){
             if(summonerName == last10Matches[k].participantIdentities[j].player.summonerName){
@@ -75,17 +93,59 @@ function displaySummoner(){
             }
         }
         let playerStats = last10Matches[k].participants[playerID].stats;
+        champsUsed[k] = last10Matches[k].participants[playerID].championId;
+        winL10+=playerStats.win==true?1:0;
         kL10+=playerStats.kills;
         dL10+=playerStats.deaths;
-        aL10+=playerStats.assits;
+        aL10+=playerStats.assists;
+        minionsL10+=playerStats.totalMinionsKilled;
+        champLvL10+=playerStats.champLevel;
+        ccL10+=playerStats.timeCCingOthers;
+        dmgL10+=playerStats.totalDamageDealt;
+        dmgL10R+=playerStats.totalDamageTaken;
+        goldL10+=playerStats.goldEarned;
+        visionL10+=playerStats.visionScore;
     }
-    let leKdaDesLastGames = document.getElementById(last10KDA);
-    let leMinionDesLastGames = document.getElementById(last10Minions);
-    let leChampLvlDesLastGames = document.getElementById(last10ChampLevel);
-    let leCcDesLastGames = document.getElementById(last10CrowdControl);
-    kL10 = Math.ceil(kL10/10);
-    dL10 = Math.ceil(dL10/10);
-    aL10 = Math.ceil(aL10/10);
+    let laLigneOuJEcris = document.getElementById("last10ligne");
+    laLigneOuJEcris.innerHTML = "";
+    winL10 = winL10*last10length;
+    laLigneOuJEcris.innerHTML += "<td>"+winL10+"%</td>";
+    kL10 = Math.ceil(kL10/last10length);
+    dL10 = Math.ceil(dL10/last10length);
+    aL10 = Math.ceil(aL10/last10length);
     kdaL10 = kL10+"/"+dL10+"/"+aL10;
-    leKdaDesLastGames.innerHTML=kdaL10;
+    laLigneOuJEcris.innerHTML += "<td>"+kdaL10+"</td>";
+    minionsL10 = Math.ceil(minionsL10/last10length);
+    laLigneOuJEcris.innerHTML += "<td>"+minionsL10+"</td>";
+    champLvL10 = Math.ceil(champLvL10/last10length);
+    laLigneOuJEcris.innerHTML += "<td>"+champLvL10+"</td>";
+    ccL10 = Math.ceil(ccL10/last10length);
+    laLigneOuJEcris.innerHTML += "<td>"+ccL10+" seconds</td>";
+    dmgL10 = Math.ceil(dmgL10/last10length);
+    laLigneOuJEcris.innerHTML += "<td>"+dmgL10+"</td>";
+    dmgL10R = Math.ceil(dmgL10R/last10length);
+    laLigneOuJEcris.innerHTML += "<td>"+dmgL10R+"</td>";
+    goldL10 = Math.ceil(goldL10/last10length);
+    laLigneOuJEcris.innerHTML += "<td>"+goldL10+"</td>";
+    visionL10 = Math.ceil(visionL10/last10length);
+    laLigneOuJEcris.innerHTML += "<td>"+visionL10+"</td>";
+
+    if(last10length != 0){
+        let laTable = document.getElementById("last10Stats");
+        laTable.visibility="visible";
+    }
+
+    for(let i = 0 ; i < last10length ; i++){
+        if(!isIn(champsTreated,champsUsed[i])){
+            champsTreated[champsTreated.length] = champsUsed[i];
+            champsTreatedRate[champsTreatedRate.length] = 1;
+        }else{
+            champsTreatedRate[champsTreated.indexOf(champsUsed[i])] = champsTreatedRate[champsTreated.indexOf(champsUsed[i])] + 1;
+        }
+    }
+    let endroitDesChampions = document.getElementById("ChampionsUsed");
+    endroitDesChampions.innerHTML = "";
+    for(let i = 0; i < champsTreated.length; i++){
+        endroitDesChampions.innerHTML += '<div><img src="./DATA/10.5.1/img/champion/'+chIdToName(champsTreated[i])+'.png" alt="Champion">'+ champsTreatedRate[i]*10+"%</div>";
+    }
 }
